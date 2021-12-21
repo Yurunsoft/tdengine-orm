1a38795cd0c8eda97fd3819f9ea51ffbe66ba4b1
Yurun <admin@yurunsoft.com>
2021-12-21 16:15:35 +0800

    增加扩展测试

--- tests/unit/ExtensionModelTest.php
+++ tests/unit/ExtensionModelTest.php
@@ -12,6 +12,10 @@ class ExtensionModelTest extends ModelTest
 {
     protected function setUp(): void
     {
+        if (!\extension_loaded('tdengine'))
+        {
+            $this->markTestSkipped();
+        }
         TDEngineManager::setDefaultClientName('test-extension');
         TDEngineOrm::setClientHandler(new Handler());
     }
