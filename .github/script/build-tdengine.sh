b69a07d4a8a51c5556f5dedf9c7ba128839357bc
Yurun <admin@yurunsoft.com>
2022-11-25 14:12:36 +0800

    test

--- .github/script/build-tdengine.sh
+++ .github/script/build-tdengine.sh
@@ -2,5 +2,5 @@
 cd /tmp && \
 git clone --recurse -b ver-${TDENGINE_VERSION} --depth=1 https://github.com/taosdata/TDengine.git && \
 cd TDengine && \
-mkdir debug && cd debug && cmake .. -DBUILD_JDBC=false -DBUILD_TOOLS=false && make -j && make install && \
+mkdir debug && cd debug && cmake .. -DBUILD_JDBC=false -DBUILD_TOOLS=false -DBUILD_HTTP=false && make -j && make install && \
 systemctl start taosd taosadapter
