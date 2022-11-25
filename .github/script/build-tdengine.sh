962ee6e544076bb3eb743a21c154fddf5c7cca3e
Yurun <admin@yurunsoft.com>
2022-11-25 14:04:14 +0800

    修复测试

--- .github/script/build-tdengine.sh
+++ .github/script/build-tdengine.sh
@@ -3,4 +3,4 @@ cd /tmp && \
 git clone --recurse -b ver-${TDENGINE_VERSION} --depth=1 https://github.com/taosdata/TDengine.git && \
 cd TDengine && \
 mkdir debug && cd debug && cmake .. -DBUILD_JDBC=false -DBUILD_TOOLS=false && make -j && make install && \
-systemctl start taosd
+systemctl start taosd taosadapter
